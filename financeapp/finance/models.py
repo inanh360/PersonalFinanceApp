4fd3579105b4c10282ce7955e77e957976f2eb73
inanh360 <inanh5177@gmail.com>
2025-02-08 18:25:14 +0000

    First Addition
    Adding my first files.

---
+++ financeapp/finance/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Months(models.Model):
+    MONTH_CHOICES = [
+        (1, 'January'),
+        (2, 'February'),
+        (3, 'March'),
+        (4, 'April'),
+        (5, 'May'),
+        (6, 'June'),
+        (7, 'July'),
+        (8, 'August'),
+        (9, 'September'),
+        (10, 'October'),
+        (11, 'November'),
+        (12, 'December'),
+    ]
+
+    month = models.IntegerField(choices=MONTH_CHOICES)
+
+    def __str__(self):
+        return f"Month: {self.get_month_display()}"
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    )
+
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    #month = models.ForeignKey(Months, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.amount} - {self.date}"
+
+class Budget(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    limit = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.category} - {self.limit}"
